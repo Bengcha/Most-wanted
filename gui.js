e24e356dbe231ad2cd503784597beefdf1ab0451
Beng Cha <bengcha04@gmail.com>
2017-01-31 10:56:29 -0600

    added Next of Kin

--- gui.js
+++ gui.js
@@ -15,7 +15,7 @@ function initSearch(people)
         SearchForImmediateFamily(people, firstName, lastName);
         break;
         case "4":
-        alert("Under Construction");
+        SearchForNextKin(people,firstName, lastName);
         break;
         case "5":
         alert("Under Construction");
@@ -95,7 +95,7 @@ function SearchForDescendant(people, firstName, lastName)
       displayDescendant(person, people);
       return true;
     }
-  else
+    else
     {
       return false;
     }
@@ -104,40 +104,39 @@ function SearchForDescendant(people, firstName, lastName)
 
 function displayDescendant(person, people)
 {
-    var descendants = getDescendants(person, people);
-    displayNameOnly(descendants);
+  var descendants = getDescendants(person, people);
+  displayNameOnly(descendants);
 }
 
 function getDescendants(person, people,counter=-1, list=[])
 {
-    if(person !== undefined)
+  if(person !== undefined)
+  {
+    var descendants = people.filter(function (descendant)
     {
-        var descendants = people.filter(function (descendant)
-        {
-            return descendant.parents.includes(person.id);
+      return descendant.parents.includes(person.id);
         });
         list.push(...descendants);
-
         counter++;
         getDescendants(list[counter], people, counter, list);
     }
-    return list;
+  return list;
 }
 
 function SearchForImmediateFamily(people, firstName, lastName)
 {
   return people.filter(function (person)
-        {
-            if (person.firstName.toLowerCase() === firstName.toLowerCase() && person.lastName.toLowerCase() === lastName.toLowerCase())
-            {
-              getImmediateFamily(person, people);
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        });
+  {
+    if (person.firstName.toLowerCase() === firstName.toLowerCase() && person.lastName.toLowerCase() === lastName.toLowerCase())
+    {
+      getImmediateFamily(person, people);
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  });
 }
 
 function getImmediateFamily(person, people)
@@ -153,76 +152,195 @@ function getFamilyMemebers(person, people)
     var spouse = getSpouse(person, people);
     var children = getChildren(person, people);
     var familyMember = [];
-
     familyMember.push(...parents);
     familyMember.push(...siblings);
     familyMember.push(...spouse);
     familyMember.push(...children);
-
     return familyMember;
 }
 
 function getParents(person, people)
 {
-    var parents = people.filter(function (family)
-    {
-        return person.parents.includes(family.id);
-    });
-    return parents;
+  var parents = people.filter(function (family)
+  {
+    return person.parents.includes(family.id);
+  }
+  );
+  return parents;
 }
 
 function getSibling(person, people)
 {
-    var sibling = people.filter(function(family)
-    {
-        return family.parents.includes(person.parents);
-    });
-    return sibling;
+  var sibling = people.filter(function(family)
+  {
+    return family.parents.includes(person.parents);
+  });
+  return sibling;
 }
 
-function getSpouse(person, people){
-    var spouse = people.filter(function (family)
-    {
-        return family.currentSpouse === person.id;
-    });
-    return spouse;
-}
-function getChildren(person, people){
-    var childrenList = people.filter(function (family)
-    {
-        return family.parents.includes(person.id);
-    });
-    return childrenList;
-}
-
-function SearchForNextKin(people)
+function getSpouse(person, people)
 {
-    if(isNaN(firstName) === true && isNaN(lastName) === true)
-    {
-        var searchPerson = people.filter(function (person)
-        {
-            if (person.firstName.toLowerCase() === firstName.toLowerCase() && person.lastName.toLowerCase() === lastName.toLowerCase())
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        });
-        if(searchPerson.length === 1)
-        {
-            getImmediateFamily(searchPerson[0], data);
-        }
-        else
-        {
+  var spouse = people.filter(function (family)
+  {
+    return family.currentSpouse === person.id;
+  });
+  return spouse;
+}
 
-            alert("No Name Found");
-        }
+function getChildren(person, people)
+{
+  var childrenList = people.filter(function (family)
+  {
+    return family.parents.includes(person.id);
+  });
+  return childrenList;
+}
+
+function getGrandChildren(person, people)
+{
+  var list = [];
+  var grandChildren = getChildren(person, people);
+  for(var children in grandChildren)
+  {
+    var grandChild = getChildren(children, people);
+    list.push(...grandChild);
+  }
+  return list;
+}
+
+function getNieceNephew(person, people)
+{
+  var list = [];
+  var sibling = getSibling(person, people);
+  for(var family in sibling)
+  {
+    var nephewNiece = getChildren(family, people);
+    list.push(...nephewNiece);
+  }
+  return list;
+}
+
+function getAuntsAndUncles(person, people)
+{
+  var list = [];
+  var parents = getParents(person, people);
+  for(var parent in parents)
+  {
+    var auntUncle = getSibling(parent, people);
+    list.push(...auntUncle);
+  }
+  return list;
+}
+
+function getGreatGrandChildren(person, people)
+{
+  var list = [];
+  var grandChildren = getGrandChildren(person, people);
+  for(var children in grandChildren)
+  {
+    var greatGrandChildren = getChildren(children, people);
+    list.push(...greatGrandChildren);
+  }
+  return list;
+}
+
+function getGrandParents(person, people)
+{
+  var list = [];
+  var getparent = getParents(person, people);
+  for(var parents in getparent)
+  {
+    var getGrandParent = getParents(parents, people);
+    list.push(...getGrandParent);
+  }
+  return list;
+}
+
+function getGreatGrandParents(person, people)
+{
+  var list =[];
+    var parents = getGrandParents(person, people);
+    for(var parent in parents)
+    {
+      var greatGrandParents = getParents(parent, people);
+      list.push(...greatGrandParents);
+    }
+  return list;
+}
+
+function SearchForNextKin(people, firstName, lastName)
+{
+  return people.filter(function (person)
+  {
+    if (person.firstName.toLowerCase() === firstName.toLowerCase() && person.lastName.toLowerCase() === lastName.toLowerCase())
+    {
+      var kin = getNextOfKin(person, people);
+      if (kin !== undefined)
+      {
+        displayNameOnly(getNextOfKin(person, people));
+        return true;
+      }
+      else
+      {
+        alert("This person have no next of kin");
+      }
     }
     else
     {
-        alert("No name Found");
-        initSearch(people);
+      return false;
     }
+  });
+}
+
+function getNextOfKin(person, people)
+{
+  var arrayOfKins = [];
+  arrayOfKins.push(getSpouse(person, people)[0]);
+  if(arrayOfKins[0] === undefined)
+  {
+    arrayOfKins.splice(0, 1);
+    arrayOfKins.push(getChildren(person, people)[0]);
+  }
+  if(arrayOfKins[0] === undefined)
+  {
+    arrayOfKins.splice(0, 1);
+    arrayOfKins.push(getParents(person, people)[0]);
+  }
+  if(arrayOfKins[0] === undefined)
+  {
+    arrayOfKins.splice(0, 1);
+    arrayOfKins.push(getSibling(person, people)[0]);
+  }
+  if(arrayOfKins[0] === undefined)
+  {
+    arrayOfKins.splice(0, 1);
+    arrayOfKins.push(getGrandChildren(person, people)[0]);
+  }
+  if(arrayOfKins[0] === undefined)
+  {
+    arrayOfKins.splice(0, 1);
+    arrayOfKins.push(getGrandParents(person, people)[0]);
+  }
+  if(arrayOfKins[0] === undefined)
+  {
+    arrayOfKins.splice(0, 1);
+    arrayOfKins.push(getNieceNephew(person, people)[0]);
+  }
+  if(arrayOfKins[0] === undefined)
+  {
+    arrayOfKins.splice(0, 1);
+    arrayOfKins.push(getAuntsAndUncles(person, people)[0]);
+  }
+  if(arrayOfKins[0] === undefined)
+   {
+    arrayOfKins.splice(0, 1);
+    arrayOfKins.push(getGreatGrandChildren(person, people)[0]);
+  }
+  if(arrayOfKins[0] === undefined)
+  {
+    arrayOfKins.splice(0, 1);
+    arrayOfKins.push(getGreatGrandParents(person, people)[0]);
+  }
+
+  return arrayOfKins;
 }
